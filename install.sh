42d7bdac2429a02a771932f4b64b1881499440ca
Christian Neverdal <flyrev@gmail.com>
2022-06-01 13:31:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,11 @@
 # Run update and upgrade if we haven't done that in a while
 [ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mtime -7)" ] && sudo apt update && sudo apt -yqq upgrade && sudo apt -yqq dist-upgrade && sudo apt -yqq autoremove
 
+sudo apt install -yqq git
 sudo apt install -yqq screen minicom ripgrep moreutils
 sudo apt install -yqq libgit2-dev
 sudo apt install -yqq rename
+sudo apt install -yqq rpl
 
 # nRF Connect Tools
 export NRF_CONNECT_TOOLS_VERSION=10.15.4
@@ -20,10 +22,8 @@ sudo dpkg -i /tmp/nrf-command-line-tools_${NRF_CONNECT_TOOLS_VERSION}_arm64.deb
 sudo mkdir -p /opt/gnuarmemb
 sudo tar xvf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 -C /opt/gnuarmemb/
 
-sudo apt -yqq install git
-
-pip3 uninstall west
-pip3 install west==0.12
+pip install west==0.12
+pip install west==0.12
 
 # GN
 mkdir ${HOME}/gn
@@ -38,6 +38,7 @@ source ${HOME}/.bashrc
 # Kitware
 wget -c https://apt.kitware.com/kitware-archive.sh
 sudo bash kitware-archive.sh
+sudo apt update
 sudo apt install -yqq git cmake ninja-build gperf \
   ccache dfu-util device-tree-compiler wget \
   python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
@@ -50,14 +51,13 @@ cd ~/ncs
 python3 -m venv env
 . env/bin/activate
 pip install --upgrade pip
-pip uninstall west
 pip install west==0.12
 west init -m https://github.com/nrfconnect/sdk-nrf --mr ${NRF_SDK_VERSION}
 west update
 west zephyr-export
-pip3 install -r zephyr/scripts/requirements.txt
-pip3 install -r nrf/scripts/requirements.txt
-pip3 install -r bootloader/mcuboot/scripts/requirements.txt
+pip install -r zephyr/scripts/requirements.txt
+pip install -r nrf/scripts/requirements.txt
+pip install -r bootloader/mcuboot/scripts/requirements.txt
 cd -
 
 } # ensure entire script is downloaded #
